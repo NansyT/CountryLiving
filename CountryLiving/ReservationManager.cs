aab3dd2eb27420a3a7cb34cba11a57fdd7cfecf9
NansyT <nanna.tronbjerg@gmail.com>
2020-05-13 10:19:00 +0200

    'f'

---
+++ CountryLiving/ReservationManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryLiving
+{
+    public class ReservationManager
+    {
+        public void CreateReservation(DateTime from, DateTime to, int roomID)
+        {
+            Debug.WriteLine("Reservation created");
+        }
+    }
+}
