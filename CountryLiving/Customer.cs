b8fb8b8b0b7fdd6e0c1b1e0f1856c417fb3c12ac
NansyT <nanna.tronbjerg@gmail.com>
2020-05-14 09:54:36 +0200

    jjj

---
+++ CountryLiving/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryLiving
+{
+    class Customer
+    {
+        private string name;
+        private string address;
+        private int zipcode;
+        private string city;
+        private int phonenumber;
+        private string email;
+        
+    }
+}
