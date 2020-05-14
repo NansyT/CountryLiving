efb2b703b99196e10c91766e693cef58e8499cbc
NansyT <nanna.tronbjerg@gmail.com>
2020-05-14 12:42:07 +0200

    Added WPF

---
+++ LandLyst/LandLyst/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using CountryLiving;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LandLyst
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new ReservationManager().CreateReservation(DateTime.Now, DateTime.Now, 0);
+        }
+    }
+}
