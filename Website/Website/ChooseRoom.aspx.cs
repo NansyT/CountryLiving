b9293b130302010bcb59373ad12c1af13fbbb58f
Philip Larsen <Kjedtoft@gmail.com>
2020-06-17 10:43:51 +0200

    Change TOBIAS FUCKING WEIRD MAIL

--- Website/Website/ChooseRoom.aspx.cs
+++ Website/Website/ChooseRoom.aspx.cs
@@ -23,7 +23,6 @@ namespace Website
         {
             StartDato.Value = DateTime.Now.ToString("dd/MM/yyyy");
             SlutDato.Value = DateTime.Now.AddDays(7).ToString("dd/MM/yyyy");
-            Session["mail"] = "tobi@gmail.com";
         }
 
 
