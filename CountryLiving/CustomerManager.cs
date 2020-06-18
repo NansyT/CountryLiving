72786fab3516b4b1a66d352fe1b50d422b7c5585
Philip Larsen <Kjedtoft@gmail.com>
2020-06-18 08:06:00 +0200

    Change reservation method in sqlmanager
    and find customer in customermanager

--- CountryLiving/CustomerManager.cs
+++ CountryLiving/CustomerManager.cs
@@ -29,38 +29,17 @@ namespace CountryLiving
             NpgsqlDataReader rdr = GetCustomerinfo(email).ExecuteReader();
             while(rdr.Read()) //for hver række der er i query 
             {
-                name = rdr.GetString(1);
-                address = rdr.GetString(2);
-                zipcode = rdr.GetInt32(3);
-                phone = rdr.GetInt32(4);
+                name = rdr[0].ToString();
+                address = rdr[1].ToString();
+                zipcode = Convert.ToInt32(rdr[2]);
+                phone = Convert.ToInt32(rdr[3]);
             }
-            Debug.WriteLine(email, name, address, zipcode, phone);
+            Debug.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}", email, name, address, zipcode, phone);
               Customer customer = new Customer(email, name, address, zipcode, phone);
                 return customer;
 
             
             
-        }
-        public void TEST(string email)
-        {
-            GetCustomerinfo(email).ExecuteReader();
-            string name = null;
-            string address = null;
-            int zipcode = 0;
-            int phone = 0;
-
-            NpgsqlDataReader rdr = GetCustomerinfo(email).ExecuteReader();
-            while (rdr.Read()) //for hver række der er i query 
-            {
-                name = rdr["fullname"].ToString();
-                address = rdr["address"].ToString();
-                zipcode = Convert.ToInt32(rdr["zipcode"]);
-                phone = Convert.ToInt32(rdr["phone"]);
-                Debug.WriteLine("{0}\n", rdr[0]);
-            }
-            Debug.WriteLine(email, name, address, zipcode, phone);
-
-
         }
     }
 }
